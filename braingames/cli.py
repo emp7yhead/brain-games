4cb361138f46548659434879222ba807481bce07
emp7yhead <artyomkropp@gmail.com>
2021-11-25 23:44:45 +0900

    feat: add linter flake8

--- braingames/cli.py
+++ braingames/cli.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+
 import prompt
+
 
 def welcome_user():
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(username))
+
 
 if __name__ == '__main__':
     welcome_user()
--- braingames/scripts/brain_games.py
+++ braingames/scripts/brain_games.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+
 from braingames.cli import welcome_user
+
 
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
