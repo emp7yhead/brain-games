3d11bf24ed78e120211605cc45a85b90ff4ceb74
emp7yhead <artyomkropp@gmail.com>
2021-11-28 01:58:14 +0900

    add brain-gcd

---
+++ braingames/games/gcd_game.py
@@ -0,0 +1,29 @@
+import random
+from braingames.functions.func import game_round
+
+LOWER_LIMIT = 1
+UPPER_LIMIT = 100
+RULE_MSG = 'Find the greatest common divisor of given numbers.'
+
+
+def game_mode():
+    random_num1 = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+    random_num2 = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+    question = str(random_num1) + ' ' + str(random_num2)
+    return str(gcd(random_num1, random_num2)), question
+
+
+def gcd(a, b):
+    '''calculate greatest common divisor
+       using Euclidean algorithm
+    '''
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return b if a == 0 else a
+
+
+def gcd_game():
+    game_round(RULE_MSG, game_mode)
---
+++ braingames/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from braingames.games.gcd_game import gcd_game
+
+
+def main():
+    gcd_game()
+
+
+if __name__ == '__main__':
+    main()
