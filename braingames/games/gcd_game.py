9d3d8140292689a9a2d064d81a8cdd2715a9bc7b
emp7yhead <artyomkropp@gmail.com>
2021-12-01 20:45:38 +0900

    fix: custom gcd changed to math.gcd

--- braingames/games/gcd_game.py
+++ braingames/games/gcd_game.py
@@ -1,4 +1,5 @@
 import random
+import math
 from braingames.game_engine.game_engine import generate_game_round
 
 LOWER_LIMIT = 1
@@ -10,20 +11,8 @@ def generate_gcd_mode():
     random_num1 = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     random_num2 = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     question = str(random_num1) + ' ' + str(random_num2)
-    correct_answer = str(gcd(random_num1, random_num2))
+    correct_answer = str(math.gcd(random_num1, random_num2))
     return correct_answer, question
-
-
-def gcd(a, b):
-    '''calculate greatest common divisor
-       using Euclidean algorithm
-    '''
-    while a != b:
-        if a > b:
-            a -= b
-        else:
-            b -= a
-    return b if a == 0 else a
 
 
 def gcd_game():
