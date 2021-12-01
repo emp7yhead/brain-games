9d5f833203c2ed3f2d4aa0c75412d53ba8f2ea32
emp7yhead <artyomkropp@gmail.com>
2021-12-01 22:31:54 +0900

    fix: is_prime now return boolean

--- braingames/games/prime_game.py
+++ braingames/games/prime_game.py
@@ -14,7 +14,9 @@ def generate_prime_mode():
 
 def is_prime(num):
     for i in range(2, num // 2 + 1):
-        return False if num % i == 0 else True
+        if num % i == 0:
+            return False 
+    return True
 
 
 def prime_game():
