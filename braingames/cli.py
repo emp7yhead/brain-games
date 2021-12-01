2925c0b12536678f5a184cfb62edfb60b92d8e55
emp7yhead <artyomkropp@gmail.com>
2021-12-01 20:38:03 +0900

    fix cli.py

--- braingames/cli.py
+++ braingames/cli.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import prompt
 
 
@@ -7,7 +5,3 @@ def welcome_user():
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(username))
-
-
-if __name__ == '__main__':
-    welcome_user()
